4f16bf35fe8e14c38a2d619493ff64008f047fbd
Steven King <stevenking86@gmail.com>
2016-04-17 20:54:17 -0400

    Creating new Users is working, created logout path

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,8 +8,19 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
+      if @user.save
+        session[:user_id] = @user.id
+      else
+        redirect_to root_path
+      end
     redirect_to root_path
   end
 
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :name)
+  end
 
 end
