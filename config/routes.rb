2667b8638211525146642fc18bd84cf9fa0e039d
Steven King <stevenking86@gmail.com>
2016-04-17 20:19:17 -0400

    Make users routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+
+
+  resources :users, only: [:show, :create, :update]
+  resources :sessions, only: [:create]
+
+
+  get 'register' => 'users#new'
+  get 'login' => 'sessions#new'
+  get 'logout' => 'sessions#destroy'
+
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
