2f8c2fc9dbac558ddaae4a07e4a885dddb104d58
Steven King <stevenking86@gmail.com>
2016-04-18 10:33:46 -0400

    Create form for logging in, add user helpers to applicationcontroller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,17 @@
 class SessionsController < ApplicationController
 
   def new
+
   end
 
   def create
+    @user = User.find_by(email: params[:email])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect_to root_path
+    else
+      redirect_to root_path
+    end
   end
 
   def destroy
