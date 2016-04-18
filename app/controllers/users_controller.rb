dfcf1b036f168cc8316706e270d1f5e1de845b39
Steven King <stevenking86@gmail.com>
2016-04-17 20:48:51 -0400

    Create form for User creation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,15 @@
 class UsersController < ApplicationController
 
+  def new
+    @user = User.new
+  end
+
+  def show
+  end
+
+  def create
+    redirect_to root_path
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
 
-  resources :users, only: [:show, :create, :update]
+  resources :users, only: [:show, :create]
   resources :sessions, only: [:create]
 
 
